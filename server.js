eb3ec07c4c11bbed828cbd79380f9b696b9cdd42
Martin Lehmann <lehmar11@student.westerdals.no>
2015-11-29 00:31:14 +0100

    Add a front page and support post names ending with .md

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs');
 const express = require('express');
 const app = express();
 
@@ -7,9 +8,26 @@ const renderTemplate = require('./TemplateRenderer');
 
 app.get('/favicon.ico', (req, res) => res.sendStatus(404));
 
+app.get('/', (req, res) => {
+  return fs.readdir(path.join(__dirname, 'posts'), (err, fileNames) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send('Something went wrong!');
+    }
+
+    const fileListHtml = '<ul>' + fileNames.map(fileName => `<li><a href="/${fileName}">${fileName}</a></li>`).join('') + '</ul>';
+    return res.send(fileListHtml);
+  });
+});
+
 app.get('/:title', (req, res) => {
-  const title = req.params.title;
-  const postPath = path.join(__dirname, 'posts', title + '.md');
+  const rawTitle = req.params.title;
+
+  const title = rawTitle.endsWith('.md')
+    ? rawTitle
+    : `${rawTitle}.md`;
+
+  const postPath = path.join(__dirname, 'posts', title);
 
   renderMarkdownFile(postPath)
     .then(renderedPost => {
