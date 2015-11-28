3f7e72e2eb2c21340f0fcb6c9fba4b855764e4d4
Martin Lehmann <lehmar11@student.westerdals.no>
2015-11-28 22:26:37 +0100

    Use index.html as a template for posts and use bootstrap from CDN.

--- server.js
+++ server.js
@@ -4,18 +4,31 @@ var express = require('express');
 var app = express();
 var marked = require('marked');
 
+app.get('/favicon.ico', function(req, res) {
+  return res.sendStatus(404);
+});
+
 app.get('/:title', function(req, res) {
   var title = req.params.title;
   var postPath = path.join(__dirname, 'posts', title + '.md');
 
-  console.log('trying to load file from path', postPath);
-
   fs.readFile(postPath, 'utf8', function(err, markdown) {
     if (err) {
+      console.log(err);
       return res.status(404).send('Could not find a post called "' + title + '.md"');
     }
 
-    res.send(marked(markdown));
+    var indexPath = path.join(__dirname, 'index.html');
+    fs.readFile(indexPath, 'utf8', function(err, template) {
+      if (err) {
+        console.log(err);
+        return res.send(500).send('Something went wrong.');
+      }
+
+      var renderedPost = marked(markdown);
+      var html = template.replace(/\{\{POSTS\}\}/g, renderedPost);
+      res.send(html);
+    });
   }); 
 });
 
